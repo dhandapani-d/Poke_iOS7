de7998b790d121f1d3e1dc848614da50e4d36568
Dhandapani Devarajan <dhandapani.kgp@gmail.com>
2014-01-07 23:00:24 +0530

    Initial Commit of a sample Tabbar based Application

---
+++ iOS7_GettingStarted/GettingStarted/GettingStarted/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GettingStarted
+//
+//  Created by Dhandapani on 07/01/14.
+//  Copyright (c) 2014 Dhandapani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS7_GettingStarted/GettingStarted/GettingStarted/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  GettingStarted
+//
+//  Created by Dhandapani on 07/01/14.
+//  Copyright (c) 2014 Dhandapani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
